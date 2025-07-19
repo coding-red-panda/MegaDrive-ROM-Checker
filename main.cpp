8704596b846c7373424bc951b993c5cecfa73e8e
Arne De Herdt <arne.de.herdt@gmail.com>
2025-07-19 11:52:19 +0200

    Fix Dependencies
    The project was not building with static linking.
    This prevented the executable from working on Windows.
    
    Also added argument parsing so the executable can be tested by just dropping a ROM file on it.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "Rom.h"
+
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        std::cout << "Loading " << argv[1] << "..." << std::endl;
+        const auto rom = Rom(argv[1]);
+        const auto copyright = rom.getCopyright();
+
+        std::cout << "Target System: " << rom.getSystemType() << std::endl;
+        std::cout << "Copyright: " << copyright.getPublisher() << std::endl;
+        std::cout << "Release Date: " << copyright.getMonth() << " " << copyright.getYear() << std::endl;
+    }
+
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get();
 }
